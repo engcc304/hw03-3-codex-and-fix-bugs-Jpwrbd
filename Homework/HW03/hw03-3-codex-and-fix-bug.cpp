a3fccb5c1c6dc25955a9add49fd556dd13db17a7
Jpwrbd <jpwrbd456@gmail.com>
2023-08-04 11:33:06 +0700

    1

--- Homework/HW03/hw03-3-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-3-codex-and-fix-bug.cpp
@@ -6,7 +6,7 @@ int main()
     int rows1, cols1, rows2, cols2, rows3, cols3, rows4, cols4;
     int i, j, k;
 
-    printf("Enter Matrix1 : \n");
+    printf("Enter Matrix1 :\n");
     printf("Enter number rows and columns : ");
     scanf("%d %d", &rows1, &cols1);
     for (i = 0; i < rows1; i++)
@@ -18,7 +18,7 @@ int main()
         }
     }
 
-    printf("\nEnter Matrix2 : \n");
+    printf("\nEnter Matrix2 :\n");
     printf("Enter number rows and columns : ");
     scanf("%d %d", &rows2, &cols2);
     for (i = 0; i < rows2; i++)
@@ -30,7 +30,7 @@ int main()
         }
     }
 
-    printf("\nMatrix1 : \n");
+    printf("\nMatrix1 :\n");
     for (i = 0; i < rows1; i++)
     {
         for (j = 0; j < cols1; j++)
@@ -40,7 +40,7 @@ int main()
         printf("\n");
     }
 
-    printf("\nMatrix2 : \n") ;
+    printf("\nMatrix2 :\n") ;
     for (i = 0; i < rows2; i++)
     {
         for (j = 0; j < cols2; j++)
@@ -58,7 +58,7 @@ int main()
             Matrix3[i][j] = Matrix1[j][i];
     }
 }
-printf("\nTranspose of Matrix1 is : \n");
+printf("\nTranspose of Matrix1 is :\n");
 for (i = 0; i < rows3; i++)
 {
     for (j = 0; j < cols3; j++)
@@ -85,7 +85,7 @@ if (cols1 != rows2){
             }
         }
     }
-    printf("\nMatrix1 x Matrix2 is : \n");
+    printf("\nMatrix1 x Matrix2 is :\n");
     for (i = 0; i < rows4; i++)
     {
         for (j = 0; j < cols4; j++)
